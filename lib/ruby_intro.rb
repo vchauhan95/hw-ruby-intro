1dca434239288ab3d189d6af9215edaf5fb7a4d4
EC2 Default User <ec2-user@ip-172-31-18-150.us-east-2.compute.internal>
2020-01-30 22:02:16 +0000

    HW code updated

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,32 +4,66 @@
 
 def sum arr
   # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  b = arr.max(2)
+  b.inject(0, :+)
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  complements = Set.new
+  0.upto(arr.length - 1) do |i|
+    value = arr[i]
+    complement = n - value
+    if complements.include?(value)
+      return true
+    else
+      complements.add(complement)
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if s =~ /\A(?=[^aeiou])(?=[a-z])/i
+    return true
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s =~ /^[10]*00$/ or s == "0"
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  attr_accessor :isbn, :price
+  def initialize isbn, price
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  def price_as_string
+    format("$%.2f", @price)
+  end
 end
